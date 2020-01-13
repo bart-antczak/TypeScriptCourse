f5dd05ce4be18bd2422101f984e88d2db5463344
bart-mant-io <antczak.bar@gmail.com>
2020-01-13 08:22:17 +0100

    Class for Project and type for Listener

--- 9_Practice/src/app.ts
+++ 9_Practice/src/app.ts
@@ -1,10 +1,21 @@
+// Project Type
+enum ProjectStatus { Active, Finished}
+
+class Project {
+    constructor(public id: string, public title: string, public description: string, public people: number, public status: ProjectStatus) {
+    }
+}
+
+type Listener = (items: Project[]) => void;
+
 // Project State Management
 class ProjectState {
-    private listeners: any[] = [];
-    private projects: any[] = [];
     private static instance: ProjectState;
+    private listeners: Listener[] = [];
+    private projects: Project[] = [];
 
-    private constructor() {}
+    private constructor() {
+    }
 
     static getInstance() {
         if (this.instance) {
@@ -14,17 +25,12 @@ class ProjectState {
         return this.instance;
     }
 
-    addListener(listenerFn: Function) {
+    addListener(listenerFn: Listener) {
         this.listeners.push(listenerFn);
     }
 
     addProject(title: string, description: string, numOfPeople: number) {
-        const newProject = {
-            id: Math.random().toString(),
-            title: title,
-            description: description,
-            people: numOfPeople
-        };
+        const newProject = new Project(Math.random().toString(), title, description, numOfPeople, ProjectStatus.Active);
         this.projects.push(newProject);
         for (const listenerFn of this.listeners) {
             listenerFn(this.projects.slice());
@@ -92,7 +98,7 @@ class ProjectList {
         this.element = importedNode.firstElementChild as HTMLElement;
         this.element.id = `${this.type}-projects`;
 
-        projectState.addListener((projects: any[]) => {
+        projectState.addListener((projects: Project[]) => {
             this.assignedProjects = projects;
             this.renderProjects()
         });
